309d083d1e33256f9d53016bc316e64feba34e7f
Roopesh <rrattan.89@gmail.com>
2017-05-16 14:41:20 -0400

    Exercice 0 and 1

---
+++ exercises.rb
@@ -0,0 +1,26 @@
+# Exercise 0
+# 1
+fav_colours = ["red", "navy blue", "black", "white", "grey"]
+age = [21, 21, 24, 25, 28]
+coin_flips = ["heads", "heads", "not", "heads", "not"]
+performing_artists = ["Kanye", "Jay-Z", "Rihanna"]
+fav_colours = [:red, :navyblue, :black, :white, :grey]
+
+# 2
+words =  { habit:"a regular tendency", alley: "a narrow back street", curve: "a continuously bending line without angles"
+}
+movies = {fightclub: 1999, matrix: 2003, harrypotter: 2001}
+cities = {toronto:2790000, losangeles: 3929000, london: 8647000}
+names_ages = {komal: 25, chantal: 21, manuj: 24}
+
+# Exercise 1
+# 1
+print coin_flips
+# 2
+print fav_colours
+# 3
+puts age
+# 4
+age << 0
+# 5
+puts movies[:fightclub]
---
+++ file.rb
@@ -0,0 +1,18 @@
+bears = ['Cheer', 'Funshine', 'Love-a-lot', 'Grumpy']
+
+bears.each do |bear|
+  puts "#{bear} Bear battles Professor Coldheart"
+end
+
+puts 'Professor Coldheart flees to the Land Without Feelings.'
+
+
+numbers = [1, 2, 3, 4, 5]
+
+numbers.each do |number|
+  puts "#{number} * #{5} is equal to #{number * 5}"
+end
+
+[1,2,3,4,5].each do |number|
+  puts "#{number} * #{5} is equal to #{number*5}"
+end
